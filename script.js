7a6ea0a558effae75fde484d95dc1eb262bf1211
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-01 10:31:04 -0300

    Chore(script.js): Change all the double ticks for simple ticks

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const tabs = document.querySelectorAll(".tabs li");
-const info = document.querySelectorAll(".info");
+const tabs = document.querySelectorAll('.tabs li');
+const info = document.querySelectorAll('.info');
 
 tabs.forEach((tab) => {
-	tab.addEventListener("click", (event) => {
+	tab.addEventListener('click', (event) => {
 		info.forEach((infoBlock) => {
 			if (infoBlock.id === tab.dataset.id) {
-				tabs.forEach((tab) => tab.classList.remove("active"));
-				info.forEach((infoBlock) => infoBlock.classList.remove("active"));
-				infoBlock.classList.add("active");
-				tab.classList.add("active");
+				tabs.forEach((tab) => tab.classList.remove('active'));
+				info.forEach((infoBlock) => infoBlock.classList.remove('active'));
+				infoBlock.classList.add('active');
+				tab.classList.add('active');
 			}
 		});
 	});
