62373e169cfa98dcea751c4d99ab4bc563e907f1
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-02 09:33:48 -0300

    fix: Change the id on the info div's to data-id

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const tabs = document.querySelectorAll('.tabs li');
 const info = document.querySelectorAll('.info');
 
 tabs.forEach((tab) => {
-	tab.addEventListener('click', (event) => {
+	tab.addEventListener('click', () => {
 		info.forEach((infoBlock) => {
-			if (infoBlock.id === tab.dataset.id) {
+			if (infoBlock.dataset.id === tab.dataset.id) {
 				tabs.forEach((tab) => tab.classList.remove('active'));
 				info.forEach((infoBlock) => infoBlock.classList.remove('active'));
 				infoBlock.classList.add('active');
